11841b21b177285083301455a68d6607a3e6d009
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-03 18:15:29 -0500

    refactor

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import streamlit as st
 import requests
 import os
 
+IMAGE_FILEPATH = "image.png"
+
 api_key = os.getenv("NASA_API_KEY")
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
-request = requests.get(url)
+response = requests.get(url)
+content = response.json()
 
-content = request.json()
 title = content["title"]
 description = content["explanation"]
-
 image_url = content["url"]
+
 image = requests.get(image_url)
-with open("image.png", "wb") as file:
+with open(IMAGE_FILEPATH, "wb") as file:
     file.write(image.content)
 
 st.title(title)
-st.image("image.png")
+st.image(IMAGE_FILEPATH)
 st.write(description)
