ca0f6776db3a5c276985ed209411dba4be50e6e1
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-03 18:08:12 -0500

    Extract title, explanation, and image from the data

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ api_key = os.getenv("NASA_API_KEY")
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
 request = requests.get(url)
-print(request)
 
 content = request.json()
+title = content["title"]
+description = content["explanation"]
 
+image_url = content["url"]
+image = requests.get(image_url)
+with open("image.png", "wb") as file:
+    file.write(image.content)
